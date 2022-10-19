1baaa228ff7ea7e4a0379ee76b9e46bbbb501933
Haniel Ubogu <hanieltobi@gmail.com>
2022-10-19 13:28:56 +0100

    Add files via upload

---
+++ _app/immutable/chunks/1-6d400c56.js
@@ -0,0 +1,1 @@
+import{default as t}from"../components/error.svelte-f2a92edd.js";export{t as component};
---
+++ _app/immutable/chunks/2-247fd011.js
@@ -0,0 +1,1 @@
+import{default as t}from"../components/pages/_page.svelte-b687d2eb.js";export{t as component};
